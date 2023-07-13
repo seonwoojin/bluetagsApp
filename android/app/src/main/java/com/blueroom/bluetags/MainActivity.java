770fdf3e8153194514becb5da3f17a6f3bea7f20
Seonwoo <wlstjsdn12@gmail.com>
2023-07-13 15:40:34 +0900

    .

--- android/app/src/main/java/com/blueroom/bluetags/MainActivity.java
+++ android/app/src/main/java/com/blueroom/bluetags/MainActivity.java
@@ -1,4 +1,4 @@
-package com.bluetags;
+package com.blueroom.bluetags;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/blueroom/bluetags/MainApplication.java
+++ android/app/src/main/java/com/blueroom/bluetags/MainApplication.java
@@ -1,4 +1,4 @@
-package com.bluetags;
+package com.blueroom.bluetags;
 
 import android.app.Application;
 import android.content.res.Configuration;
